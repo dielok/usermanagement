4f7eacdcdda246b62f472f0f011ff538d4fa1045
Martin Leue <martinleue@localhost>
2017-11-07 15:02:41 +0100

    Update finished

--- src/User/UserController.php
+++ src/User/UserController.php
@@ -4,6 +4,7 @@ namespace User;
 use Helper\Salt;
 use Helper\Cleaner;
 use Exception;
+use User\UserModel;
 use User\UserRepository;
 
 class UserController {
@@ -28,7 +29,7 @@ class UserController {
         $lastname   = $data['lastname'];
         $firstname  = $data['firstname'];
         
-        $checkEmail = $this->userRepository->findMail($data['email']);
+        $checkEmail = $this->userRepository->findMail($email);
         
         if(empty($checkEmail) AND filter_var($email, FILTER_VALIDATE_EMAIL) !== false){
             if(strlen($password) >= 8){
@@ -95,13 +96,65 @@ class UserController {
         return $data;
     }
     
-    public function getUserdata($user_id){
-        $user = $this->userRepository->getUserData($user_id);
+    public function getUserdata($user_id,$session_id){
+        $user = $this->userRepository->getUserData($user_id);      
         
-        
-        if(empty($user)){
+        if(empty($user) || $user->session_id != $session_id){
             throw new Exception();
         }
         return $user;
+    }
+    
+    public function userUpdate($user_id,$post,$session_id){
+        foreach($post as $key => $value){
+            $data[$key] = $this->cleaner->params($value);
+        } 
+        
+        $pwSalt = $this->userRepository->getUserData($user_id);
+
+        $dataUpdate['user_id'] = $user_id;
+        if(isset($data['email'])){
+            $d .= "email = :email,";
+            $dataUpdate['email']=$data['email'];
+        }
+        if(isset($data['password'])){
+            $d .= "password = :password,";
+            $dataUpdate['password']= password_hash($data['password'].$pwSalt->salt, PASSWORD_DEFAULT);
+        }
+        if(isset($data['lastname'])){
+            $d .= "lastname = :lastname,";
+            $dataUpdate['lastname']=$data['lastname'];
+        }
+        if(isset($data['firstname'])){
+            $d .= "firstname = :firstname,";
+            $dataUpdate['firstname']=$data['firstname'];
+        }   
+        $d = substr($d, 0, (strlen($d)-1));
+        
+        $this->userRepository->userUpdate($user_id, $d, $dataUpdate);
+        $user = $this->userRepository->getUserData($user_id);
+        
+        if(empty($user) || $user->session_id != $session_id){
+            throw new Exception();
+        }
+        return $user;
+    }
+    
+    public function deleteUser($user_id, $post){
+        $password = $this->cleaner->params($post['password']);
+        
+        $user = $this->userRepository->getUserData($user_id);  
+        
+        if(password_verify($password.$user->salt, $user->password)){
+            $this->userRepository->deleteUser($user_id);
+            $data = [
+                "Report"    => "Success",
+                "Msg"       => "User was deleted",
+                "Status"    => 200
+            ];
+        }else{
+            throw new Exception();
+        }
+        return $data;
     }
 }
