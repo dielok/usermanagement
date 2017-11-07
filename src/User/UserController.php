9704af5e5499b8bba23fd9ef39d9cea05a9867fe
Martin Leue <martinleue@localhost>
2017-11-07 11:19:03 +0100

    Register finished

--- src/User/UserController.php
+++ src/User/UserController.php
@@ -1,52 +1,45 @@
 <?php
-/**
- * Description of LoginController
- *
- * @author martinleue
- */
 namespace User;
 
 use Helper\Salt;
-use Helper\Token;
 use Helper\Cleaner;
 use User\UserRepository;
 
 class UserController {
 
-    public $pdo, $cleaner, $userRepository, $token, $salt;
+    public $pdo, $cleaner, $userRepository, $salt;
 
     public function __construct(UserRepository $userRepository) {
         $this->userRepository = $userRepository;
         $this->cleaner = new Cleaner();
         $this->salt = new Salt();
-        $this->token = new Token();
     }
     
-    public function register($post){
+    public function register($post, $session_id){
         
         foreach($post as $key => $value){
             $data[$key] = $this->cleaner->params($value);
         }
         
-        $salt = $this->salt->salt();
-        $email = $data['email'];
-        $this->token->init($email, $salt);
-        $token = $this->token->newToken();
-        $password = password_hash($data['password'].$salt, PASSWORD_DEFAULT);
-        $lastname = $data['lastname'];
-        $firstname = $data['firstname'];
+        $salt       = $this->salt->salt();
+        $email      = $data['email'];
+        $password   = password_hash($data['password'].$salt, PASSWORD_DEFAULT);
+        $lastname   = $data['lastname'];
+        $firstname  = $data['firstname'];
         
         $checkEmail = $this->userRepository->findMail($data['email']);
         
         if(empty($checkEmail) AND filter_var($email, FILTER_VALIDATE_EMAIL) !== false){
             if(strlen($password) >= 8){
-                if(strlen($lastname) >= 1 && strlen($firstname) >= 1){
-                    $register = $this->userRepository->register($email, $password, $lastname, $firstname, $salt, $token);
+                if((strlen($lastname) && strlen($firstname)) >= 1){
+                    
+                    $register = $this->userRepository->register($email, $password, $lastname, $firstname, $salt, $session_id);
+                    
                     $data = [
                         "Report"    => "Success",
                         "Msg"       => "User successfully registered",
                         "Status"    => 201,
-                        "Token"     => $token
+                        "Token"     => $session_id
                     ];                    
                 }else{
                     $data = [
@@ -72,34 +65,32 @@ class UserController {
         return $data;
     }
     
-    public function signin($post){
-
+    public function signin($post,$session_id){
+        
         foreach($post as $key => $value){
             $data[$key] = $this->cleaner->params($value);
         }
         
-        $email = $data['email'];
-        $password = $data['password'];
+        $email      = $data['email'];
+        $password   = $data['password'];
         
-        $checkUser = $this->userRepository->signin($email);
+        $newSessionId   = $this->userRepository->newSessionId($email, $session_id);
+        $checkUser      = $this->userRepository->signin($email);   
         
         if(password_verify(($password.$checkUser->salt), $checkUser->password)){
-            $token = $this->userRepository->getToken($checkUser->user_id);
             $data = [
                 "Report"    => "Success",
                 "Msg"       => "Registration was successful",
                 "Status"    => 201,
-                "Token"     => $token
+                "Token"     => $checkUser->session_id
             ];
-        }
-        else{
+        }else{
             $data = [
                 "Report"    => "Failure",
                 "Msg"       => "Login failed",
                 "Status"    => 200
             ];
         }
-        
         return $data;
     }
 }
